4e51afa7e1eb73d5a4b918b8321af20e853d236f
trikitrok <trikitrok@gmail.com>
2024-02-27 00:39:45 +0100

    removing unnecessary fields

--- 04-refactoring-to-hexagonal-architecture/src/infrastructure/EmailNotSentError.ts
+++ 04-refactoring-to-hexagonal-architecture/src/infrastructure/EmailNotSentError.ts
@@ -1,7 +1,4 @@
 export class EmailNotSentError extends Error {
-    public message: string;
-    public name: string;
-
     constructor(err: Error) {
         super("email not sent: " + err.message)
     }
