acae06f7209166fb32b69b6583f520753df4c716
trikitrok <trikitrok@gmail.com>
2024-02-27 14:10:22 +0100

    removing coupling with date format used in production

--- 04-refactoring-to-hexagonal-architecture/test/helper/OurDateFactory.ts
+++ 04-refactoring-to-hexagonal-architecture/test/helper/OurDateFactory.ts
@@ -1,6 +1,10 @@
 import {OurDate} from "../../src/core/OurDate";
-import {DateRepresentation} from "../../src/infrastructure/repositories/DateRepresentation";
 
 export function date(dateAsString: string): OurDate {
-    return new DateRepresentation(dateAsString).toDate();
+    const [year, month, day] = dateAsString.split("/");
+    if([year, month, day].some((value) => value === undefined)) {
+        throw new Error("wrong date format in test");
+    }
+    const date = new Date(Number(year), Number(month) - 1, Number(day));
+    return new OurDate(date);
 }
