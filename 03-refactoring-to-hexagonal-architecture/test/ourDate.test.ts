b7299a347ee02d8e7cce970ff92bf9c51ff16ebe
freyes <fran@codesai.com>
2024-11-20 15:19:57 +0000

    fix test

--- 03-refactoring-to-hexagonal-architecture/test/ourDate.test.ts
+++ 03-refactoring-to-hexagonal-architecture/test/ourDate.test.ts
@@ -7,7 +7,7 @@ describe('OurDate', () => {
     const ourDate = new OurDate("1789/01/24");
     const sameDay = new OurDate("2001/01/24");
     const notSameDay = new OurDate("1789/01/25");
-    const notSameMonth = new OurDate("1789/02/25");
+    const notSameMonth = new OurDate("1789/02/24");
 
     expect(ourDate.isSameDay(sameDay)).toBeTruthy(); //"same"
     expect(ourDate.isSameDay(notSameDay)).toBeFalsy(); //"not same day"
